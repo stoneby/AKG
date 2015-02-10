bcd52f30a3c68029a2dce0c48b6c113af7a461a4
cui xiaoliang <stonebyme@gmail.com>
2015-02-10 14:11:58 +0800

    couple of updates.
    - add border phy material , bouncing on the lfet right borders.
    - tune trigger switch on or off effect.
    - adding unity vs plugin.

---
+++ Assets/Scripts/FlowingGroundController.cs
@@ -0,0 +1,36 @@
+﻿using AnimationOrTween;
+using UnityEngine;
+
+public class FlowingGroundController : MonoBehaviour
+{
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.LogWarning("On trigger endter");
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        Debug.LogWarning("On trigger exit");
+        collider2D.isTrigger = false;
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.LogWarning("On collision enter.");
+
+        var rigid = other.rigidbody;
+        var velocityUp = rigid.velocity.y > 0;
+        var center = collider2D.bounds.center;
+        var size = collider2D.bounds.size / 2;
+        var colliderCenter = rigid.collider2D.bounds.center;
+        var colliderSize = rigid.collider2D.bounds.size / 2;
+
+        //collider2D.isTrigger = true;
+    }
+
+    void OnCollisionExit2D(Collision2D other)
+    {
+        Debug.LogWarning("On collision exit");
+    }
+}
--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -1,130 +1,169 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 public class PlayerControl : MonoBehaviour
 {
-	[HideInInspector]
-	public bool facingRight = true;			// For determining which way the player is currently facing.
-	[HideInInspector]
-	public bool jump = false;				// Condition for whether the player should jump.
-	[HideInInspector]
-	public bool fire = false;
-	
-	public float h { get; set; }
+    [HideInInspector]
+    public bool facingRight = true;			// For determining which way the player is currently facing.
+    [HideInInspector]
+    public bool jump = false;				// Condition for whether the player should jump.
+    [HideInInspector]
+    public bool fire = false;
 
-	public enum InputDevice
-	{
-		HUD,
-		Keyboard,
-	};
+    public float h { get; set; }
 
-	public InputDevice inputDevice;
-	public AbstractInput inputManager;
+    public enum InputDevice
+    {
+        HUD,
+        Keyboard,
+    };
 
-	public float horizontalSpeed = 5f;		// The fastest the player can travel in the x axis.
-	public float verticalSpeed = 5f;
-	public AudioClip[] jumpClips;			// Array of clips for when the player jumps.
+    public InputDevice inputDevice;
+    public AbstractInput inputManager;
 
-	private Transform groundCheck;			// A position marking where to check if the player is grounded.
-	private bool grounded = false;			// Whether or not the player is grounded.
-	private Animator anim;					// Reference to the player's animator component.
+    public float horizontalSpeed = 5f;		// The fastest the player can travel in the x axis.
+    public float verticalSpeed = 5f;
+    public AudioClip[] jumpClips;			// Array of clips for when the player jumps.
 
-	void Awake()
-	{
-		// Setting up references.
-		groundCheck = transform.Find("groundCheck");
-		anim = GetComponent<Animator>();
+    private Transform groundCheck;			// A position marking where to check if the player is grounded.
+    private bool grounded;			        // Whether or not the player is grounded.
+    private Animator anim;					// Reference to the player's animator component.
+    private bool bordered;                  // Whether or not the player is in bordered.
 
-		if(inputDevice == InputDevice.HUD)
-		{
-			inputManager = GetComponent<HUDInput>();
-		}
-		else
-		{
-			inputManager = GetComponent<VirtualInput>();
-		}
-	}
+    void Awake()
+    {
+        // Setting up references.
+        groundCheck = transform.Find("groundCheck");
+        anim = GetComponent<Animator>();
 
-	void Update()
-	{
-		// The player is grounded if a linecast to the groundcheck position hits anything on the ground layer.
-		var lastgrounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));  
+        if (inputDevice == InputDevice.HUD)
+        {
+            inputManager = GetComponent<HUDInput>();
+        }
+        else
+        {
+            inputManager = GetComponent<VirtualInput>();
+        }
+    }
 
-		if(lastgrounded != grounded)
-		{
-			grounded = lastgrounded;
+    void Update()
+    {
+        // The player is grounded if a linecast to the groundcheck position hits anything on the ground layer.
+        var lastgrounded = Physics2D.Linecast(transform.position, groundCheck.position, LayerMask.GetMask("Ground"));
 
-			collider2D.isTrigger = !grounded;
-		}
+        if (lastgrounded != grounded)
+        {
+            grounded = lastgrounded;
 
-		//collider2D.isTrigger = !(grounded);
+            collider2D.isTrigger = !grounded && !bordered;
+        }
 
-		// If the jump button is pressed and the player is grounded then the player should jump.
-		if(inputManager.DoesJump() && grounded)
-		{
-			jump = true;
-		}
+        //collider2D.isTrigger = !(grounded);
 
-		if(inputManager.DoesFire() && grounded)
-		{
-			fire = true;
-		}
-	}
+        // If the jump button is pressed and the player is grounded then the player should jump.
+        if (inputManager.DoesJump() && grounded)
+        {
+            jump = true;
+        }
 
-
-	void FixedUpdate ()
-	{
-		// Cache the horizontal input.
-		h = inputManager.GetHorizontal();
+        if (inputManager.DoesFire() && grounded)
+        {
+            fire = true;
+        }
+    }
 
 
-		// The Speed animator parameter is set to the absolute value of the horizontal input.
-		anim.SetFloat("Speed", Mathf.Abs(h));
+    void FixedUpdate()
+    {
+        // Cache the horizontal input.
+        h = inputManager.GetHorizontal();
 
-		h = Mathf.Clamp(h, -1, 1);
-		rigidbody2D.velocity = new Vector2(horizontalSpeed * h, rigidbody2D.velocity.y);
+        // The Speed animator parameter is set to the absolute value of the horizontal input.
+        anim.SetFloat("Speed", Mathf.Abs(h));
 
-		// If the input is moving the player right and the player is facing left...
-		if(h > 0 && !facingRight)
-			// ... flip the player.
-			Flip();
-		// Otherwise if the input is moving the player left and the player is facing right...
-		else if(h < 0 && facingRight)
-			// ... flip the player.
-			Flip();
+        h = Mathf.Clamp(h, -1, 1);
+        rigidbody2D.velocity = new Vector2(horizontalSpeed * h, rigidbody2D.velocity.y);
 
-		// If the player should jump...
-		if(jump)
-		{
-			// Set the Jump animator trigger parameter.
-			anim.SetTrigger("Jump");
+        // If the input is moving the player right and the player is facing left...
+        if (h > 0 && !facingRight)
+            // ... flip the player.
+            Flip();
+        // Otherwise if the input is moving the player left and the player is facing right...
+        else if (h < 0 && facingRight)
+            // ... flip the player.
+            Flip();
 
-			rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, verticalSpeed);
+        // If the player should jump...
+        if (jump)
+        {
+            // Set the Jump animator trigger parameter.
+            anim.SetTrigger("Jump");
 
-			// Play a random jump audio clip.
-			//int i = Random.Range(0, jumpClips.Length);
-			//AudioSource.PlayClipAtPoint(jumpClips[i], transform.position);
+            rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, verticalSpeed);
 
-			// Make sure the player can't jump again until the jump conditions from Update are satisfied.
-			jump = false;
-		}
+            // Play a random jump audio clip.
+            //int i = Random.Range(0, jumpClips.Length);
+            //AudioSource.PlayClipAtPoint(jumpClips[i], transform.position);
 
-		if(fire)
-		{
-			anim.SetTrigger("Attack");
-			fire = false;
-		}
-	}
-	
-	
-	void Flip ()
-	{
-		// Switch the way the player is labelled as facing.
-		facingRight = !facingRight;
+            // Make sure the player can't jump again until the jump conditions from Update are satisfied.
+            jump = false;
+        }
 
-		// Multiply the player's x local scale by -1.
-		Vector3 theScale = transform.localScale;
-		theScale.x *= -1;
-		transform.localScale = theScale;
-	}
+        if (fire)
+        {
+            anim.SetTrigger("Attack");
+            fire = false;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.LogWarning("On trigger enter." + other.name);
+
+        if (other.tag.Equals("Borders"))
+        {
+            bordered = true;
+            collider2D.isTrigger = false;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        Debug.LogWarning("On trigger exit." + other.name);
+
+        if (other.tag.Equals("Borders"))
+        {
+            bordered = false;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.LogWarning("On collision enter." + other.transform.name);
+
+        if (other.collider.tag.Equals("Borders"))
+        {
+            bordered = true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D other)
+    {
+        Debug.LogWarning("On collision exit." + other.transform.name);
+
+        if (other.collider.tag.Equals("Borders"))
+        {
+            bordered = false;
+        }
+    }
+
+    void Flip()
+    {
+        // Switch the way the player is labelled as facing.
+        facingRight = !facingRight;
+
+        // Multiply the player's x local scale by -1.
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
 }
