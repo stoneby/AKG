9e6eea5a5562f6dff7c0f198c1e4756d91ff1d83
cui xiaoliang <stonebyme@gmail.com>
2015-04-10 13:32:48 +0800

    人和怪交叉的时候可以打击。

--- Assets/Scripts/Character/Hero/PlayerAttackController.cs
+++ Assets/Scripts/Character/Hero/PlayerAttackController.cs
@@ -1,81 +1,95 @@
 ﻿using System.Collections;
 using UnityEngine;
 
-public class PlayerAttackController : MonoBehaviour 
+public class PlayerAttackController : MonoBehaviour
 {
-	public float AttackInterval;
+    public float AttackInterval;
 
-	private PlayerControl player;
-	private CharacterCommon characterCommon;
-	private CharacterInformation playerInfor;
+    private PlayerControl player;
+    private CharacterCommon characterCommon;
+    private CharacterInformation playerInfor;
 
-	private bool monsterInRange;
+    private bool monsterInRange;
 
-	void OnTriggerEnter2D(Collider2D other)
-	{
+    void OnTriggerEnter2D(Collider2D other)
+    {
         Debug.LogWarning("PlayerAttackController OnTriggerEnter2D: " + other.name + ", parent: " + ((other.transform.parent == null) ? "root" : other.transform.parent.name));
-        
+
         if (other.tag.Equals("Monster"))
-		{
+        {
             StartCoroutine("CheckAttack", other);
-		}
-	}
-	
-	void OnTriggerExit2D(Collider2D other)
-	{
-		Debug.LogWarning("PlayerAttackController OnTriggerExit2D: " + other.name);
+        }
+    }
 
-		if(other.tag.Equals("Monster"))
-		{
+    void OnTriggerExit2D(Collider2D other)
+    {
+        Debug.LogWarning("PlayerAttackController OnTriggerExit2D: " + other.name);
+
+        if (other.tag.Equals("Monster"))
+        {
             StopCoroutine("CheckAttack");
-		}
-	}
+        }
+    }
 
-	IEnumerator CheckAttack(Collider2D other)
-	{
-		while (true)
-		{
-			yield return null;
+    IEnumerator CheckAttack(Collider2D other)
+    {
+        while (true)
+        {
+            yield return null;
 
-			// in case monster is dead.
-			if (other == null)
-			{
-				break;
-			}
-
-            var facingRight = (other.transform.position.x > player.transform.position.x);
-            var monsterCommon = other.GetComponent<CharacterCommon>();
-            if (characterCommon.FacingRight == facingRight && !monsterCommon.IsDead)
-		    {
-                if (player.fire)
-                {
-                    HurtMonster(other);
-
-                    yield return new WaitForSeconds(AttackInterval);
-                }
+            // in case monster is dead for a while.
+            if (other == null)
+            {
+                break;
             }
-		}
-	}
+
+            // monster is dead but not destroyed.
+            var monsterCommon = other.GetComponent<CharacterCommon>();
+            if (monsterCommon.IsDead)
+            {
+                break;
+            }
+
+            // player is not firing.
+            if (!player.fire)
+            {
+                continue;
+            }
+
+            // not in the same side and not overlap borders.
+            var facingRight = (other.transform.position.x > player.transform.position.x);
+            var playerBorder = player.collider2D.bounds;
+            var monsterBorder = monsterCommon.collider2D.bounds;
+            if (characterCommon.FacingRight != facingRight && !playerBorder.Intersects(monsterBorder))
+            {
+                continue;
+            }
+
+            // right place to hurt monster.
+            HurtMonster(other);
+            yield return new WaitForSeconds(AttackInterval);
+        }
+    }
 
     void HurtMonster(Collider2D other)
     {
         // health update.
         var monster = other.GetComponent<MonsterControll>();
-		monster.Hurt();
+        monster.Hurt();
 
         // monster ui update.
         var monsterHealth = other.GetComponent<CharacterHealth>();
         monsterHealth.Hurt();
-        
+
         // player ui update.
         var playerInfor = player.GetComponent<CharacterInformation>();
-		playerInfor.Show(true);
-	}
+        playerInfor.Show(true);
+    }
 
-	void Awake()
-	{
-		player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerControl>();
-		characterCommon = player.GetComponent<CharacterCommon>();
-		playerInfor = player.GetComponent<CharacterInformation>();
-	}
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerControl>();
+        characterCommon = player.GetComponent<CharacterCommon>();
+        playerInfor = player.GetComponent<CharacterInformation>();
+    }
 }
