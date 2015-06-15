c7cd8a2daf20275c8dd557a4f6a88214d7a02250
cui xiaoliang <stonebyme@gmail.com>
2015-06-15 18:51:33 +0800

    tile in.

---
+++ Assets/Scripts/Character/Map/MapGenerator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+    public List<string> TileList;
+
+    public Texture2D Texture;
+
+    private List<Sprite> tileSpriteList = new List<Sprite>();
+
+    [ContextMenu("Execute")]
+    public void Generate()
+    {
+        foreach (var tileName in TileList)
+        {
+            var sprite = new Sprite();
+            //sprite.texture = Texture;
+            sprite.name = tileName;
+
+            tileSpriteList.Add(sprite);
+        }
+    }
+
+    [ContextMenu("Cleanup")]
+    public void Cleanup()
+    {
+        tileSpriteList.ForEach(DestroyImmediate);
+        tileSpriteList.Clear();
+    }
+
+    void Awake()
+    {
+        
+    }
+}
