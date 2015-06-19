fda4ce9125dcdeb26173395f7eb9ca91fcfbcc91
cui xiaoliang <stonebyme@gmail.com>
2015-06-19 17:03:40 +0800

    tower prefab 50%.

--- Assets/Scripts/Character/TowerState/TowerAttackState.cs
+++ Assets/Scripts/Character/TowerState/TowerAttackState.cs
@@ -2,43 +2,24 @@
 
 public class TowerAttackState : MonoBehaviour
 {
+    public PowerEffectController PowerEffectController;
     public GameObject AttackPrefab;
 
-    private MonsterControll monster;
+    public float ShootBeginTime;
+    public float ShootInterval;
+
     private PlayerControl player;
-	private CharacterInformation monsterInfor;
-	private CharacterCommon monsterCommon;
-	private CharacterCommon playerCommon;
+    private CharacterCommon monsterCommon;
 
     private Transform attackLocation;
     private AttackEffectController attackEffectController;
-
-    void OnEnable()
-    {
-		//playerCommon.Hurt();
-		//monsterInfor.Show(true);
-    }
-
-    void FixedUpdate()
-    {
-        rigidbody2D.velocity = Vector2.zero;
-
-		if (monsterCommon.FacingRight && player.transform.position.x < monster.transform.position.x)
-        {
-			monsterCommon.Flip();
-        }
-
-		if (!monsterCommon.FacingRight && player.transform.position.x > monster.transform.position.x)
-        {
-			monsterCommon.Flip();
-        }
-    }
+    private Animator powerAnimator;
 
     /// <summary>
     /// Attack right place occurs.
     /// </summary>
     /// <remarks>Refers to attack animation right place.</remarks>
-    public void AttackRightPlace()
+    public void OnShoot(GameObject go)
     {
         GenerateEffect();
 
@@ -46,28 +27,47 @@ public class TowerAttackState : MonoBehaviour
         attackEffectController.Play();
     }
 
-
-    void Awake()
-    {
-		player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerControl>();
-		playerCommon = player.GetComponent<CharacterCommon>();
-		monster = GetComponent<MonsterControll>();
-		monsterCommon = GetComponent<CharacterCommon>();
-		monsterInfor = GetComponent<CharacterInformation>();
-
-        attackLocation = transform.Find("Effect/Attack");
-    }
-
     private void GenerateEffect()
     {
         var attackObject = Instantiate(AttackPrefab, attackLocation.position, attackLocation.rotation) as GameObject;
         attackEffectController = attackObject.GetComponent<AttackEffectController>();
-		attackEffectController.Owner = gameObject;
+        attackEffectController.Owner = gameObject;
 
         //attackObject.transform.parent = attackLocation;
         attackObject.transform.localScale =
             new Vector3(
                 monsterCommon.FacingRight ? attackObject.transform.localScale.x : -attackObject.transform.localScale.x,
                 attackObject.transform.localScale.y, attackObject.transform.localScale.z);
+    }
+
+    void PrepareToShoot()
+    {
+        powerAnimator.Play("TowerPower");
+    }
+
+    void OnEnable()
+    {
+        PowerEffectController.gameObject.SetActive(true);
+        InvokeRepeating("PrepareToShoot", ShootBeginTime, ShootInterval);
+    }
+
+    void OnDisable()
+    {
+        CancelInvoke("PrepareToShoot");
+        PowerEffectController.gameObject.SetActive(false);
+    }
+
+    void Awake()
+    {
+		player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerControl>();
+		player.GetComponent<CharacterCommon>();
+		GetComponent<MonsterControll>();
+		monsterCommon = GetComponent<CharacterCommon>();
+		GetComponent<CharacterInformation>();
+
+        attackLocation = transform.Find("Effect/Attack");
+
+        PowerEffectController.OnShoot += OnShoot;
+        powerAnimator = PowerEffectController.GetComponent<Animator>();
     }
 }
---
+++ Assets/Scripts/Effect/PowerEffectController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class PowerEffectController : MonoBehaviour
+{
+    public delegate void Shoot(GameObject go);
+
+    public Shoot OnShoot;
+
+    public void ToShoot()
+    {
+        if (OnShoot != null)
+        {
+            OnShoot(gameObject);
+        }
+    }
+}
