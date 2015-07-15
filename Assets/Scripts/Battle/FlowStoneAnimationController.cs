2b8aee47a15c4bb0abcfcbf56762716d11f72f6d
cui xiaoliang <stonebyme@gmail.com>
2015-07-15 19:16:32 +0800

    Flowing stone.

---
+++ Assets/Scripts/Battle/FlowStoneAnimationController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class FlowStoneAnimationController : MonoBehaviour
+{
+    public float StartTime;
+
+    private Animator animator;
+
+    void Flowting()
+    {
+        animator.enabled = true;
+    }
+
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+        animator.enabled = false;
+    }
+
+    void Start()
+    {
+        Invoke("Flowting", StartTime);
+    }
+}
