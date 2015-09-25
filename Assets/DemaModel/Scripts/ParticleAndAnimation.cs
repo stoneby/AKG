a02f908e520bdbb2244475b73b14d691e8cfd98c
cui xiaoliang <stonebyme@gmail.com>
2015-09-25 15:26:11 +0800

    clean

--- Assets/DemaModel/Scripts/ParticleAndAnimation.cs
+++ Assets/DemaModel/Scripts/ParticleAndAnimation.cs
@@ -3,42 +3,42 @@ using System.Collections;
 
 public class ParticleAndAnimation : MonoBehaviour
 {
-	void Start () 
-	{	
-		PlayOnce();
-	}
-	
-	[ContextMenu("Play Loop")]
-	public void PlayLoop()
-	{
-		ParticleSystem[] pss = GetComponentsInChildren<ParticleSystem>(true);
-		foreach(ParticleSystem ps in pss)
-		{
-			ps.loop = true;
-			ps.Play();
-		}
-		Animation[] anis = GetComponentsInChildren<Animation>(true);
-		foreach(Animation an in anis)
-		{
-			an.wrapMode = WrapMode.Loop;
-			an.Play();
-		}
-	}
-	
-	[ContextMenu("Play Once")]
-	public void PlayOnce () 
-	{	
-		ParticleSystem[] pss = GetComponentsInChildren<ParticleSystem>(true);
-		foreach(ParticleSystem ps in pss)
-		{
-			ps.loop = false;
-			ps.Play();
-		}
-		Animation[] anis = GetComponentsInChildren<Animation>(true);
-		foreach(Animation an in anis)
-		{
-			an.wrapMode = WrapMode.Once;
-			an.Play();
-		}
-	}
+    void Start()
+    {
+        PlayOnce();
+    }
+
+    [ContextMenu("Play Loop")]
+    public void PlayLoop()
+    {
+        var pss = GetComponentsInChildren<ParticleSystem>(true);
+        foreach (var ps in pss)
+        {
+            ps.loop = true;
+            ps.Play();
+        }
+        var anis = GetComponentsInChildren<Animation>(true);
+        foreach (var an in anis)
+        {
+            an.wrapMode = WrapMode.Loop;
+            an.Play();
+        }
+    }
+
+    [ContextMenu("Play Once")]
+    public void PlayOnce()
+    {
+        var pss = GetComponentsInChildren<ParticleSystem>(true);
+        foreach (var ps in pss)
+        {
+            ps.loop = false;
+            ps.Play();
+        }
+        var anis = GetComponentsInChildren<Animation>(true);
+        foreach (var an in anis)
+        {
+            an.wrapMode = WrapMode.Once;
+            an.Play();
+        }
+    }
 }
