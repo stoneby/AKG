8501cfa874bd7ae80b807476735ca707a559864e
Taylor Liang <10people@users.noreply.github.com>
2015-10-21 14:02:27 +0800

    add test code.

---
+++ Assets/testincompany.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class testincompany : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        ;
+        ;
+        ;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
