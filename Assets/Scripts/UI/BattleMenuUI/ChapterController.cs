498a24bd62a552b7a104196b65d486bbeacfa5e2
Taylor Liang <bu213200@gmail.com>
2015-10-31 18:33:28 +0800

    Pre-add battle menu module.

---
+++ Assets/Scripts/UI/BattleMenuUI/ChapterController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ChapterController : MonoBehaviour
+{
+    public Button ChapterButton;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void Awake()
+    {
+        if (ChapterButton == null)
+        {
+        }
+    }
+}
---
+++ Assets/Scripts/UI/BattleMenuUI/TollController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TollController : MonoBehaviour
+{
+    public Button TollButton;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
